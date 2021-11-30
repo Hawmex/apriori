f8eca70657307f806b04dde8ac87c9cadde1baf8
Hawmex <hawmed.a@gmail.com>
2021-11-30 17:04:35 +0330

    improved

--- bin/apriori.dart
+++ bin/apriori.dart
@@ -4,36 +4,30 @@ import 'dart:io';
 import 'package:apriori/apriori.dart';
 import 'package:apriori/src/options.dart';
 
-void main() {
-  transformRules();
-  sortRules();
-  writeRules();
-}
+void main(final List<String> arguments) {
+  final Options options = Options.fromDecodedJson(
+    jsonDecode(
+      File(arguments[0]).readAsStringSync(),
+    ),
+  );
 
-final List<Map<String, dynamic>> rules = [];
+  final List<Map<String, dynamic>> rules = [];
 
-final Options options = Options.fromDecodedJson(
-  jsonDecode(
-    File('options.json').readAsStringSync(),
-  ),
-);
+  final List<List<String>> transactions = (jsonDecode(
+    File('${arguments[0]}/../${options.transactionsPath}').readAsStringSync(),
+  ) as List)
+      .map((transaction) => (transaction as List).cast<String>())
+      .toList()
+      .cast<List<String>>();
 
-final List<List<String>> transactions = (jsonDecode(
-  File(options.transactionsPath).readAsStringSync(),
-) as List)
-    .map((transaction) => (transaction as List).cast<String>())
-    .toList()
-    .cast<List<String>>();
+  final Apriori apriori = Apriori(
+    transactions: transactions,
+    minSupport: options.minSupport,
+    minConfidence: options.minConfidence,
+    maxAntecedentsLength: options.maxAntecedentsLength,
+    logger: true,
+  );
 
-final Apriori apriori = Apriori(
-  transactions: transactions,
-  minSupport: options.minSupport,
-  minConfidence: options.minConfidence,
-  maxAntecedentsLength: options.maxAntecedentsLength,
-  logger: true,
-);
-
-void transformRules() {
   for (final Map<String, dynamic> rule in apriori.rules) {
     final Map<String, dynamic> currentRule = {};
 
@@ -43,14 +37,10 @@ void transformRules() {
 
     rules.add(currentRule);
   }
-}
 
-void sortRules() {
   rules.sort((a, b) => (b['lift'] as double).compareTo((a['lift'] as double)));
-}
 
-void writeRules() {
-  File(options.rulesPath).writeAsStringSync(
+  File('${arguments[0]}/../${options.rulesPath}').writeAsStringSync(
     const JsonEncoder.withIndent('  ').convert(rules),
   );
 }
--- test/apriori_test.dart
+++ test/apriori_test.dart
@@ -1,1 +1,7 @@
-void main() {}
+import 'package:test/scaffolding.dart';
+
+import '../bin/apriori.dart' as app;
+
+void main() {
+  test('App Runs With Exit Code 0', () => app.main(['test/options.json']));
+}
