52719150d1bd55f520b38832900bb0ad511f2bd3
Hawmex <hawmed.a@gmail.com>
2022-01-12 11:57:29 +0330

    improved

--- test/apriori_test.dart
+++ test/apriori_test.dart
@@ -3,8 +3,7 @@ import 'package:test/scaffolding.dart';
 import '../bin/apriori.dart' as app;
 
 void main() {
-  test(
-    'App Runs With Exit Code 0',
-    () => app.main(['test/options.json']),
-  );
+  test('App Runs With Exit Code 0', () {
+    app.main(['test/options.json']);
+  });
 }
